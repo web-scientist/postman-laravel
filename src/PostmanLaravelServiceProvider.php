5cb7c2d6bcde5b0bd2ec8cf7c661a56cb35c7d18
Chinmay Purav <70144052+chinmaypurav@users.noreply.github.com>
2021-12-10 22:38:02 +0530

    initial commit

---
+++ src/PostmanLaravelServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace WebScientist\PostmanLaravel;
+
+use Illuminate\Support\ServiceProvider;
+
+class PostmanLaravelServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
---
+++ src/Services/CollectionService.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace WebScientist\LaravelPostman\Services;
+
+
+class CollectionService
+{
+    //
+}
